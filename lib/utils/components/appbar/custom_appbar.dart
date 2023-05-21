93c3a46706c3ac48b21c4759fc90b3302d7266ed
Chandra <ardhyachandrax@gmail.com>
2023-05-21 19:31:18 +0700

    feat: pull from Dev branch

---
+++ lib/utils/components/appbar/custom_appbar.dart
@@ -0,0 +1,119 @@
+import 'package:capstone_project/utils/my_color.dart';
+import 'package:capstone_project/utils/my_size.dart';
+import 'package:flutter/material.dart';
+import '../text_box/search_text_box.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final bool action;
+  final bool searchField;
+  final bool tabBar;
+  final bool home;
+  final String title;
+  final List<Widget>? actions;
+  final List<Widget>? tabs;
+  final SearchTextBox? searchTextBox;
+
+  const CustomAppBar({
+    super.key,
+    required this.preferredSize,
+    required this.home,
+    required this.title,
+    required this.action,
+    required this.searchField,
+    required this.tabBar,
+    this.searchTextBox,
+    this.actions,
+    this.tabs,
+  });
+
+  @override
+  final Size preferredSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return PreferredSize(
+      preferredSize: preferredSize,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          AppBar(
+            toolbarHeight: 40,
+            title: Text(
+              title,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+                color: MyColor.neutralHigh,
+              ),
+            ),
+            actions: actions,
+          ),
+          home == true
+              ? Padding(
+                  padding: const EdgeInsets.only(left: MySize.screenPadding),
+                  child: Text(
+                    'Hi,SUser!',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      color: MyColor.secondaryMain,
+                    ),
+                  ),
+                )
+              : const SizedBox(),
+          searchField == true
+              ? const SizedBox(
+                  height: 16,
+                )
+              : const SizedBox(),
+          home == true
+              ? const SizedBox(
+                  height: 24,
+                )
+              : const SizedBox(),
+          searchField == true || home == true
+              ? Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: searchTextBox)
+              : const SizedBox(),
+          const SizedBox(
+            height: 31,
+          ),
+          Divider(
+            height: 1,
+            color: MyColor.neutralLow,
+            thickness: .5,
+          ),
+          tabBar == true
+              ? SizedBox(
+                  height: 60,
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: TabBar(
+                      dividerColor: Colors.transparent,
+                      isScrollable: true,
+                      indicatorWeight: 1,
+                      indicatorColor: MyColor.primaryMain,
+                      labelColor: MyColor.primaryMain,
+                      labelPadding:
+                          const EdgeInsets.symmetric(horizontal: 16.0),
+                      labelStyle: const TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w500,
+                      ),
+                      unselectedLabelColor: MyColor.neutralHigh,
+                      unselectedLabelStyle: const TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w500,
+                      ),
+                      tabs: tabs!,
+                    ),
+                  ),
+                )
+              : const SizedBox(),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/view/screen/transaction/transaction_screen.dart
@@ -0,0 +1,152 @@
+import 'package:capstone_project/utils/components/appbar/custom_appbar.dart';
+import 'package:capstone_project/utils/components/buttons/primary_button.dart';
+import 'package:capstone_project/utils/components/buttons/primary_button_icon.dart';
+import 'package:capstone_project/utils/components/text_box/search_text_box.dart';
+import 'package:capstone_project/utils/components/text_box/text_box.dart';
+import 'package:capstone_project/utils/my_focus_node.dart';
+import 'package:capstone_project/utils/my_reg_exp.dart';
+import 'package:capstone_project/utils/my_size.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class TransactionScreen extends StatefulWidget {
+  const TransactionScreen({super.key});
+
+  static const String routeName = '/transaction_screen';
+
+  @override
+  State<TransactionScreen> createState() => _TransactionScreenState();
+}
+
+class _TransactionScreenState extends State<TransactionScreen> {
+  final TextEditingController _searchController = TextEditingController();
+  final TextEditingController _pertamaController = TextEditingController();
+  final TextEditingController _keduaController = TextEditingController();
+  final TextEditingController _terakhirController = TextEditingController();
+  final FocusNode _pertamaNode = FocusNode();
+  final FocusNode _keduaNode = FocusNode();
+  final FocusNode _ketigaNode = FocusNode();
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: CustomAppBar(
+          preferredSize: Size(
+            MySize.screenWidth(context),
+            double.maxFinite,
+          ),
+          home: false,
+          action: false,
+          title: 'Transaction',
+          searchField: true,
+          tabBar: true,
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 16),
+              child: PrimaryButtonIcon(
+                teks: 'teks',
+                widget: const Icon(Icons.add),
+                onPressed: () {},
+                customHeight: 36,
+                customWidth: 100,
+              ),
+            )
+          ],
+          tabs: const [
+            Tab(
+              text: 'Ongoing',
+            ),
+            Tab(
+              text: 'History',
+            ),
+          ],
+          searchTextBox:
+              SearchTextBox(textEditingController: _searchController),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: TabBarView(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TextBox(
+                        textEditingController: _pertamaController,
+                        hintText: 'pertama',
+                        autoFocus: true,
+                        currentFocus: _pertamaNode,
+                        errorMaxLines: 1,
+                        hintMaxLines: 1,
+                        maxLength: 10,
+                        maxLines: 1,
+                        last: false,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.allow(MyRegExp.huruf),
+                          FilteringTextInputFormatter.allow(MyRegExp.spasi),
+                        ],
+                        onFieldSubmitted: (value) {
+                          MyFocusNode.change(
+                            context: context,
+                            currentFocus: _pertamaNode,
+                            nextFocus: _keduaNode,
+                          );
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                      TextBox(
+                        textEditingController: _keduaController,
+                        hintText: 'kedua',
+                        currentFocus: _keduaNode,
+                        last: false,
+                        width: 100,
+                        obscureText: true,
+                        obscureChar: '',
+                        keyboardType: TextInputType.number,
+                        maxLines: 1,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.allow(MyRegExp.angka),
+                        ],
+                        onFieldSubmitted: (value) {
+                          MyFocusNode.change(
+                              context: context,
+                              currentFocus: _keduaNode,
+                              nextFocus: _ketigaNode);
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                      TextBox(
+                        textEditingController: _terakhirController,
+                        hintText: 'terakhir',
+                        last: true,
+                        currentFocus: _ketigaNode,
+                        maxLines: 4,
+                        textCapitalization: TextCapitalization.words,
+                        validator: (value) {
+                          if (_terakhirController.text.isEmpty) {
+                            return 'Harus diisi';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                      const PrimaryButton(
+                        teks: 'Done',
+                        onPressed: null,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const Center(child: Text('teks')),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
