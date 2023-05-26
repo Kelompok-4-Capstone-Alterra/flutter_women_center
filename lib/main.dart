589a16a2dd39f73889a4d9362934d8959feb5cac
Chandra <ardhyachandrax@gmail.com>
2023-05-26 22:00:28 +0700

    feat: add configuration status bar & system nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,20 @@
-import 'package:capstone_project/view/screen/article_list/article_list_screen.dart';
+import 'package:capstone_project/view/screen/article/article_list/article_list_screen.dart';
+import 'package:capstone_project/view/screen/article/article_list/article_list_view_model.dart';
+import 'package:capstone_project/view/screen/home/home_screen.dart';
+import 'package:capstone_project/view/screen/landing/landing_screen.dart';
+import 'package:capstone_project/view/screen/landing/landing_view_model.dart';
+import 'package:capstone_project/view/screen/profile/profile_screen.dart';
+import 'package:capstone_project/view/screen/saved/acticle/article_screen.dart';
+import 'package:capstone_project/view/screen/saved/acticle/article_view_model.dart';
+import 'package:capstone_project/view/screen/saved/detail_reading_list/detail_reading_list_screen.dart';
+import 'package:capstone_project/view/screen/saved/detail_reading_list/detail_reading_list_view_model.dart';
+import 'package:capstone_project/view/screen/saved/saved_screen.dart';
+import 'package:capstone_project/view/screen/saved/saved_view_model.dart';
+import 'package:capstone_project/view/screen/transaction/transaction_screen.dart';
+import 'package:capstone_project/view/screen/transaction/transaction_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,14 +24,50 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.light(useMaterial3: true),
-      initialRoute: ArticleListScreen.routename,
-      routes: {
-        ArticleListScreen.routename: (context) => const ArticleListScreen(),
-        //ArticleDetails.routename:(context) => const ArticleDetails(articles: '')
-      },
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.white,
+      statusBarIconBrightness: Brightness.dark,
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ));
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => LandingViewModel(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => TransactionViewModel(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => SavedViewModel(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => DetailReadingListViewmodel(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => ArticleViewModel(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => ArticleListProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Woman Center',
+        theme: ThemeData.light(useMaterial3: true),
+        initialRoute: LandingScreen.routeName,
+        routes: {
+          LandingScreen.routeName: (context) => const LandingScreen(),
+          HomeScreen.routeName: (context) => const HomeScreen(),
+          TransactionScreen.routeName: (context) => const TransactionScreen(),
+          SavedScreen.routeName: (context) => const SavedScreen(),
+          ProfileScreen.routeName: (context) => const ProfileScreen(),
+          ArticleListScreen.routename: (context) => const ArticleListScreen(),
+          DetailReadingListScreen.routeName: (context) =>
+              const DetailReadingListScreen(),
+          ArticleScreen.routeName: (context) => const ArticleScreen(),
+        },
+      ),
     );
   }
 }
