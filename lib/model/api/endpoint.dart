0e81649b3317cc5a37587a3077993a627c86d3c6
rnwxyz <jamesstrom78@gmail.com>
2023-06-23 15:46:05 +0800

    fix : karena ad perubahan endpoint API

--- lib/model/api/endpoint.dart
+++ lib/model/api/endpoint.dart
@@ -1,11 +1,14 @@
 class Endpoint {
   static const String baseUrl = 'https://13.210.163.192:8080';
-  static const String login = '/login';
-  static const String verify = '/verify';
-  static const String register = '/register';
-  static const String logout = '/logout';
+  static const String login = '/users/auth/login';
+  static const String verify = '/users/auth/verify';
+  static const String checkUsernameEmail = '/users/auth/verify/unique';
+  static const String register = '/users/auth/register';
   static const String userProfile = '/users/profile';
   static const String userChangePassword = '/users/profile/password';
   static const String readingLists = '/users/reading-lists';
   static const String transacton = '/users/transactions';
+  static const String publicArticles = '/users/public/articles';
+  static const String publicCounselor = '/users/public/counselors';
+  static const String publicCareer = '/users/public/careers';
 }
