ea785a73fba1819b75c00df8c7951328d9147cc5
Chandra <ardhyachandrax@gmail.com>
2023-06-23 17:58:11 +0700

    feat: add endpoint

--- lib/model/api/endpoint.dart
+++ lib/model/api/endpoint.dart
@@ -1,11 +1,12 @@
 class Endpoint {
   static const String baseUrl = 'https://13.210.163.192:8080';
-  static const String login = '/login';
-  static const String verify = '/verify';
-  static const String register = '/register';
-  static const String logout = '/logout';
+  static const String login = '/users/auth/login';
+  static const String verify = '/users/auth/verify';
+  static const String checkUsernameEmail = '/users/auth/verify/unique';
+  static const String register = '/users/auth/register';
   static const String userProfile = '/users/profile';
-  static const String getTopics = '/topics';
+  static const String userChangePassword = '/users/profile/password';
+  static const String getTopics = '/users/public/topics';
   static const String getArticles = '/users/articles';
   static const String getArticlesNoLogin = '/users/public/articles';
   static const String getPostComment = '/users/articles/:id/comments';
@@ -14,5 +15,5 @@ class Endpoint {
   static const String readingLists = '/users/reading-lists';
   static const String postArticleToReadingLists = '/users/reading-lists/save';
   static const String deleteArticleFromReadingLists =
-      '/users/reading-lists/:id';
+      '/users/reading-lists/save/:id';
 }
