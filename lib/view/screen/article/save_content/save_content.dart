8367c7780b302b49f9af321eed7c1b6b3b29f670
archndrx <94846278+archndrx@users.noreply.github.com>
2023-06-24 20:00:42 +0700

    Feat integration article and comment (#41)
    * feat: integrat article & comment
    
    * fix: delete unused folder

---
+++ lib/view/screen/article/save_content/save_content.dart
@@ -0,0 +1,241 @@
+import 'package:capstone_project/utils/components/buttons/primary_button.dart';
+
+import 'package:capstone_project/utils/components/loading/loading.dart';
+import 'package:capstone_project/utils/components/modal_bottom_sheet/custom_bottom_sheet_builder.dart';
+import 'package:capstone_project/utils/my_color.dart';
+import 'package:capstone_project/utils/components/text_box/text_box.dart';
+import 'package:capstone_project/utils/state/finite_state.dart';
+import 'package:capstone_project/view/screen/article/article_list/article_list_post/article_list_post_view_model.dart';
+import 'package:capstone_project/view/screen/article/save_content/save_content_view_model.dart';
+import 'package:capstone_project/view/screen/saved/saved_view_model.dart';
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class SaveContent extends StatefulWidget {
+  final String articleId;
+  const SaveContent({super.key, required this.articleId});
+
+  @override
+  State<SaveContent> createState() => _SaveContentState();
+}
+
+class _SaveContentState extends State<SaveContent> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration.zero, () {
+      final provider = Provider.of<SaveContentProvider>(context, listen: false);
+      provider.showAllReadingList();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final TextEditingController listNameController = TextEditingController();
+    final TextEditingController descriptionController = TextEditingController();
+    final GlobalKey<FormState> addReadingListFormKey = GlobalKey<FormState>();
+    final FocusNode listNameNode = FocusNode();
+    final FocusNode descriptionNode = FocusNode();
+    final provider = Provider.of<SaveContentProvider>(context, listen: false);
+
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        maxHeight: MediaQuery.of(context).size.height * 0.6,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 16),
+            child: TextButton(
+              onPressed: () {
+                showModalBottomSheet(
+                    isDismissible: false,
+                    isScrollControlled: true,
+                    context: context,
+                    builder: (context) {
+                      return CustomBottomSheetBuilder(
+                        tinggi: MediaQuery.of(context).size.height * 0.8,
+                        header: true,
+                        judul: 'Create New List',
+                        cancelEvent: () {
+                          listNameController.clear();
+                          descriptionController.clear();
+                          Navigator.pop(context);
+                        },
+                        isi: [
+                          Form(
+                            key: addReadingListFormKey,
+                            child: SizedBox(
+                              height: 680,
+                              width: double.infinity,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'List Name',
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w400,
+                                      color: MyColor.neutralHigh,
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    height: 10,
+                                  ),
+                                  TextBox(
+                                    textEditingController: listNameController,
+                                    hintText:
+                                        'Ex : How to heal my traumatized inner child',
+                                    currentFocus: listNameNode,
+                                    validator: (value) {
+                                      if (value == null || value.isEmpty) {
+                                        return 'Wajib diisi';
+                                      }
+                                      return null;
+                                    },
+                                  ),
+                                  const SizedBox(
+                                    height: 16,
+                                  ),
+                                  Text(
+                                    'Description (optional)',
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w400,
+                                      color: MyColor.neutralHigh,
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    height: 10,
+                                  ),
+                                  TextBox(
+                                    textEditingController:
+                                        descriptionController,
+                                    last: true,
+                                    hintText: 'Ex : This is an important list',
+                                    currentFocus: descriptionNode,
+                                    validator: (value) {
+                                      if (value == null || value.isEmpty) {
+                                        return 'Wajib diisi';
+                                      }
+                                      return null;
+                                    },
+                                  ),
+                                  const SizedBox(
+                                    height: 16,
+                                  ),
+                                  Consumer<SaveContentProvider>(
+                                      builder: (context, savedProvider, _) {
+                                    return PrimaryButton(
+                                      teks: 'Add',
+                                      onPressed: () async {
+                                        if (addReadingListFormKey.currentState!
+                                            .validate()) {
+                                          final provider =
+                                              Provider.of<SaveContentProvider>(
+                                                  context,
+                                                  listen: false);
+                                          await provider.createReadingList(
+                                            context,
+                                            name: listNameController.text,
+                                            description:
+                                                descriptionController.text,
+                                          );
+
+                                          listNameController.clear();
+                                          descriptionController.clear();
+
+                                          await savedProvider
+                                              .refreshReadingList();
+                                        }
+                                      },
+                                    );
+                                  }),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      );
+                    }).then((value) {
+                  Provider.of<SaveContentProvider>(context, listen: false)
+                      .showAllReadingList();
+                });
+              },
+              style: TextButton.styleFrom(
+                padding: EdgeInsets.zero,
+                minimumSize: Size.zero,
+                tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              ),
+              child: Text(
+                "Create new list ...",
+                style: TextStyle(fontSize: 16, color: MyColor.secondaryPressed),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Consumer<SaveContentProvider>(
+              builder: (context, provider, _) {
+                if (provider.myState == MyState.loading) {
+                  return const Loading();
+                } else if (provider.myState == MyState.loaded &&
+                    provider.readingList.isEmpty) {
+                  return const Center(
+                    child: Text('No Reading List Yet'),
+                  ); // Tampilkan loading widget jika masih dalam proses loading
+                } else if (provider.myState == MyState.loaded &&
+                    provider.readingList.isNotEmpty) {
+                  return ListView.builder(
+                    itemCount: provider.readingList.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        onTap: () async {
+                          await provider.toggleCheck(index);
+                          provider.setSelectedReadingListId(
+                              provider.readingList[index].id ?? '');
+                        },
+                        contentPadding: const EdgeInsets.all(0),
+                        title: Text(
+                          provider.readingList[index].name ?? '',
+                          style: const TextStyle(fontSize: 16),
+                        ),
+                        trailing: provider.isItemChecked(index)
+                            ? Icon(Icons.check, color: MyColor.primaryMain)
+                            : null,
+                      );
+                    },
+                  );
+                } else {
+                  return const Center(
+                    child: Text('Failed to load data'),
+                  ); // Tampilkan pesan gagal jika gagal memuat data
+                }
+              },
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              bottom: MediaQuery.of(context).viewInsets.bottom,
+            ),
+            child: PrimaryButton(
+              teks: 'Save',
+              onPressed: () {
+                Provider.of<ArticleListPostProvider>(context, listen: false)
+                    .toggleArticleSaved(widget.articleId);
+                provider.saveToReadingList(
+                    widget.articleId, provider.selectedReadingListId);
+                provider.isButtonPressed.value = true;
+                Navigator.pop(context);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/view/screen/article/save_content/save_content_view_model.dart
@@ -0,0 +1,142 @@
+import 'package:capstone_project/model/reading_list_model.dart';
+import 'package:capstone_project/model/service/article_service.dart';
+import 'package:capstone_project/model/service/reading_list_service.dart';
+import 'package:capstone_project/utils/state/finite_state.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SaveContentProvider extends ChangeNotifier {
+  MyState myState = MyState.initial;
+  List<ReadingListModel> readingList = [];
+  String selectedReadingListId = '';
+  ValueNotifier<bool> isButtonPressed = ValueNotifier<bool>(false);
+  int checkedIndex = -1;
+  final ArticleService _articleService = ArticleService();
+  final ReadingListService _readingListService = ReadingListService();
+  late SharedPreferences _loginData;
+  String _message = '';
+
+  void changeState(MyState state) {
+    myState = state;
+    notifyListeners();
+  }
+
+  SaveContentProvider() {
+    initializeSharedPreferences();
+    loadSavedArticleIds(); // Menambahkan method ini
+  }
+
+  void loadSavedArticleIds() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    savedArticleIds = prefs.getKeys().whereType<String>().toList();
+  }
+
+  void initializeSharedPreferences() async {
+    changeState(MyState.loading);
+    _loginData = await SharedPreferences.getInstance();
+    changeState(MyState.loaded);
+  }
+
+  void setSelectedReadingListId(String readingListId) {
+    selectedReadingListId = readingListId;
+    notifyListeners();
+  }
+
+  Future<void> saveArticleStatus(String articleId, bool isSaved) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(articleId, isSaved);
+  }
+
+  Future<void> showAllReadingList() async {
+    try {
+      myState = MyState.loading;
+      notifyListeners();
+      _loginData = await SharedPreferences.getInstance();
+      final token = _loginData.getString('token') ?? '';
+      readingList = await _readingListService.getAllReadingList(token: token);
+      myState = MyState.loaded;
+      notifyListeners();
+    } catch (e) {
+      _message = e.toString();
+      myState = MyState.failed;
+      notifyListeners();
+    }
+  }
+
+  Future<void> saveToReadingList(String articleId, String readingListId) async {
+    try {
+      changeState(MyState.loading);
+      _loginData = await SharedPreferences.getInstance();
+      final token = _loginData.getString('token') ?? '';
+      await _readingListService.postArticleToReadingList(
+          token: token, articleId: articleId, readingListId: readingListId);
+      changeState(MyState.loaded);
+    } catch (e) {
+      _message = e.toString();
+      changeState(MyState.failed);
+    }
+  }
+
+  Future<void> toggleCheck(int index) async {
+    if (checkedIndex == index) {
+      checkedIndex =
+          -1; // Batal mengcentang item jika sudah dicentang sebelumnya
+    } else {
+      checkedIndex = index; // Mengcentang item yang dipilih
+    }
+    notifyListeners();
+  }
+
+  bool isItemChecked(int index) {
+    return checkedIndex == index;
+  }
+
+  Future<void> createReadingList(BuildContext context,
+      {required String name, required String description}) async {
+    try {
+      changeState(MyState.loading);
+      _loginData = await SharedPreferences.getInstance();
+      final token = _loginData.getString('token') ?? '';
+      await _readingListService.postReadingList(
+          token: token, name: name, description: description);
+      await showAllReadingList();
+      if (context != null && context.findRenderObject() != null) {
+        Navigator.pop(context); // Tutup bottom sheet jika masih ada
+      }
+    } catch (e) {
+      _message = e.toString();
+      changeState(MyState.failed);
+    }
+  }
+
+  List<String> savedArticleIds = [];
+
+  void toggleArticleSaved(String articleId) {
+    if (savedArticleIds.contains(articleId)) {
+      savedArticleIds.remove(articleId);
+      saveArticleStatus(articleId,
+          false); // Menyimpan status false ke dalam SharedPreferences
+    } else {
+      savedArticleIds.add(articleId);
+      saveArticleStatus(
+          articleId, true); // Menyimpan status true ke dalam SharedPreferences
+    }
+    notifyListeners();
+  }
+
+  bool isArticleSaved(String articleId) {
+    return savedArticleIds.contains(articleId);
+  }
+
+  Future<void> refreshReadingList() async {
+    try {
+      myState = MyState.loading;
+      final token = _loginData.getString('token') ?? '';
+      await _readingListService.getAllReadingList(token: token);
+      myState = MyState.loaded;
+    } catch (e) {
+      _message = e.toString();
+      myState = MyState.failed;
+    }
+  }
+}
